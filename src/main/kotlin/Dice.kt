77940cfd1a374bc07a1df9451c8492a630dd0eb9
greenhandzdl <greenhandzdl@gmail.com>
2023-01-28 14:13:01 +0800

    0.0.2M

--- src/main/kotlin/Dice.kt
+++ src/main/kotlin/Dice.kt
@@ -2,18 +2,20 @@ package com.greenhandzdl.dice
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import net.mamoe.mirai.event.events.GroupMemberEvent
+import net.mamoe.mirai.contact.NormalMember
+import net.mamoe.mirai.contact.isBotMuted
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
-import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.message.data.Dice
-import net.mamoe.mirai.message.data.content
-import net.mamoe.mirai.message.data.messageChainOf
+import net.mamoe.mirai.message.data.MessageSource.Key.recall
 import net.mamoe.mirai.utils.info
 import java.io.File
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import kotlin.random.Random
+
 
 object Dice : KotlinPlugin(
     JvmPluginDescription(
@@ -39,23 +41,37 @@ object Dice : KotlinPlugin(
                 file.createNewFile()
             }
 
-            when {
-                message.contentToString() == "rd" -> {
-                    val r = Random.nextInt(1,7)
-                    group.sendMessage(Dice(r))
-                    file.appendText("${sender.nick}(${sender.id.toString()}) 在${LocalDateTime.now()}扔出点数为$r \n")
-                }
-                message.contentToString() == "rd history" -> {
-                    group.sendMessage("${file.readText()}")
-                }
-                message.contentToString() == "rd clean" ->{
-                    if(sender.id.toString() != admin) {
-                        group.sendMessage("你没有相应权限操作")
-                    } else{
-                        file.delete()
+            if(group.isBotMuted){
+                file.delete()
+            }else{
+                when {
+                    message.contentToString() == "rd" -> {
+                        val r = Random.nextInt(1, 7)
+                        group.sendMessage(Dice(r))
+                        file.appendText("${sender.nick}(${sender.id.toString()}) 在${LocalDate.now()} ${LocalTime.now()}扔出点数为$r \n")
+                    }
+
+                    message.contentToString() == "rd history" -> {
+                        val forward: ForwardMessage = buildForwardMessage {
+                            add(bot.id, bot.nick, PlainText("${file.readText()}"))
+                        }
+                        subject.sendMessage(forward)
+                    }
+
+                    message.contentToString() == "rd clean" -> {
+                        if (sender.id.toString() != admin) {
+                            group.sendMessage("你没有相应权限操作")
+                        } else {
+                            file.delete()
+                        }
                     }
                 }
             }
         }
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+        logger.info{"Dice disabled"}
     }
 }
